0ac2e1918c5c99dc9fb6ef877dfe40245a4d0fa8
Kam1st <kam.hatteea@hotmail.com>
2022-12-10 20:53:00 -0500

    everything is done, get and insert is working but cant get dummy data from the code

--- src/main/java/com/um/inventoryservice/DataLayer/StockItemRepository.java
+++ src/main/java/com/um/inventoryservice/DataLayer/StockItemRepository.java
@@ -1,6 +1,8 @@
 package com.um.inventoryservice.DataLayer;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockItemRepository extends ReactiveMongoRepository<StockItem, String> {
 }
