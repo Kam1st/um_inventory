a32e3a2e5b1ace264efc3b17c41d01789c4e53b6
Stacey Jin <jin.stacey@gmail.com>
2022-12-15 22:01:15 -0500

    Added method to Service, ServiceImpl, Repository and Controller.

--- src/main/java/com/um/inventoryservice/DataLayer/StockItemRepository.java
+++ src/main/java/com/um/inventoryservice/DataLayer/StockItemRepository.java
@@ -2,10 +2,12 @@ package com.um.inventoryservice.DataLayer;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface StockItemRepository extends ReactiveMongoRepository<StockItem, String> {
     Mono<StockItem> findStockItemByStockItemId(String stockItemId);
+    Flux<StockItem> findStockItemsByPrice(double price);
     Mono<Void> deleteStockItemByStockItemId(String stockItemId);
 }
