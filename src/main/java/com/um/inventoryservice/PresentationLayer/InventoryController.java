911368005e852264b2d597c41ac9ab12000b48af
Stacey Jin <jin.stacey@gmail.com>
2022-12-18 00:07:26 -0500

    Added methods to Service, ServiceImpl, Repository and Controller.

--- src/main/java/com/um/inventoryservice/PresentationLayer/InventoryController.java
+++ src/main/java/com/um/inventoryservice/PresentationLayer/InventoryController.java
@@ -5,6 +5,7 @@ import com.um.inventoryservice.DataLayer.InventoryItem;
 import com.um.inventoryservice.DataLayer.InventoryItemDTO;
 import com.um.inventoryservice.DataLayer.StockItemDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -21,6 +22,13 @@ public class InventoryController {
         return inventoryItemService.getAll();
     }
 
+    @GetMapping("{inventoryItemId}")
+    public Mono<ResponseEntity<InventoryItemDTO>> getInventoryItemById(@PathVariable String inventoryItemId) {
+        return inventoryItemService
+                .getInventoryItemById(inventoryItemId)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
     @PostMapping
     public Mono<InventoryItemDTO> insertInventoryItem(@RequestBody Mono<InventoryItemDTO> inventoryItemDTO) {
         return inventoryItemService.insertInventoryItem(inventoryItemDTO);
