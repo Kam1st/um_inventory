8c455b5dc87f41f7243d813a529a6d3e70e7db04
Kam1st <kam.hatteea@hotmail.com>
2023-01-05 21:24:37 -0500

    added controller methods

---
+++ src/main/java/com/um/inventoryservice/PresentationLayer/OrderController.java
@@ -0,0 +1,35 @@
+package com.um.inventoryservice.PresentationLayer;
+
+import com.um.inventoryservice.BusinessLayer.OrderService;
+import com.um.inventoryservice.DataLayer.OrderDTO;
+import com.um.inventoryservice.DataLayer.StockItemDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping()
+    public Flux<OrderDTO> getAllOrders() {
+        return orderService.getAll();
+    }
+
+    @PostMapping
+    public Mono<OrderDTO> insertOrder(@RequestBody Mono<OrderDTO> orderDTOMono) {
+        return orderService.insertOrder(orderDTOMono);
+    }
+
+    @GetMapping("/price/{price}")
+    public Flux<OrderDTO> getOrdersByStockItemId(@PathVariable String stockItemId) {
+        return orderService.getOrdersByStockItemId(stockItemId);
+    }
+
+
+}
